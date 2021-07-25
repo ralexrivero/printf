b115304bc0773e0494a847cf4bb4adb77f023a46
Your Name <you@example.com>
2021-07-25 09:50:46 -0700

    posible printf con el switch a las funciones de putforprintf

---
+++ PRINTFsketch.c
@@ -0,0 +1,40 @@
+/* hola soy colo empece con esto porque me tranque con los putchat,
+ * hice solo que imprimiera una cadena y lo que ya habia hecho. */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (format)
+	{
+		va_start(list, format);
+		for (i = 0; format[i] != NULL; i++)
+		{
+			if (format[i] != '%')
+			{
+				_putchar(format[i]);
+			}
+			if (format[i] == '%')
+			{
+				switch (format[i + 1])
+				{
+					case 's':
+						cout = p_str(va_arg(list,char *));
+						break;
+					case 'd':
+						cout = p_int(va_arg(list,int));
+						break;
+					case '%':
+						cout = _putchar(37);
+						break;
+					case '\n':
+						cout = _putchar('\n');
+						break;
+				}
+			}
+		}
+		va_end(list);
+	}
+	return (cout);
+}
