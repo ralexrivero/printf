10ce8524b63415bba2cb920e3d0c42b28dff5211
Your Name <you@example.com>
2021-07-26 10:28:09 -0700

    copila con el main que nos dan

---
+++ ronalmain.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("Hola");
+	_putchar('\n');
+	_printf("Hola Colo, Joaco y Ronald");
+	_putchar('\n');
+	_printf("Hola % Holberton");
+	_putchar('\n');
+	_printf("Hola %% porcentaje");
+	_putchar('\n');
+	_printf("Salto de linea \n");
+	_putchar('\n');
+	_printf("%%");
+	_printf("hola %c", 58);
+	_printf(NULL);
+	_printf("%s", NULL);
+	return (0);
+}
