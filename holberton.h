cb670f8de4ab33030cc6ae9306a00a0bfce5deb3
root <ralexrivero@gmail.com>
2021-07-26 07:42:30 -0700

    first test

--- holberton.h
+++ holberton.h
@@ -1,31 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-/* Libraries */
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/* Prototype Putchar*/
+#include <unistd.h>
+#include <limits.h>
+#include <float.h>
+
 int _putchar(char c);
 
-/* Protptypes */
-
-int _printf(const char *format, ...);
-
-/**
- * struct - the format after %
- * @pm: parameter entered
- * @format: the format to apply
- */
-
-typedef struct
-{
-	char *pm;
-	void (*format)(va_list p);
-} formating;
-
-/* Basic prototypes for _printf */
-
-void per(va_list p);
 #endif
-
---
+++ printf.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+    int len = 0;
+	// va_list list;
+
+	if (format)
+	{
+		// va_start(list, format);
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] != '%')
+			{
+				_putchar(format[i]);
+                len++;
+			}
+			if (format[i] == '%')
+			{
+				switch (format[i + 1])
+				{
+					case 's':
+						// _putchar(p_str(va_arg(list,char *)));
+						break;
+					case 'd':
+						// _putchar(p_int(va_arg(list,int)));
+						break;
+					case '%':
+						_putchar(37);
+						break;
+					case '\n':
+						_putchar('\n');
+						break;
+				}
+			}
+		}
+		// va_end(list);
+	}
+	return (len);
+}
