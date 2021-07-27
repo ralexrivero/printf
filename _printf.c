7866d3eaecbd3bb77033ec09dfbd5a68999f553f
Your Name <you@example.com>
2021-07-27 12:20:19 -0700

    BUENO BETTY

--- _printf.c
+++ _printf.c
@@ -15,8 +15,9 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (;(format && format[i]); i++)
-		(format[i] == '%') ? (len += specifier(&i, format, list)) : (len += _putchar(format[i]));
+	for (; (format && format[i]); i++)
+	(format[i] == '%') ? (len += specifier(&i, format, list))
+		: (len += _putchar(format[i]));
 		va_end(list);
 	return (len);
 }
