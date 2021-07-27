adf6720fa2349db625012f84a2b6879e79b42cfb
ralexrivero <ralexrivero@gmail.com>
2021-07-27 14:22:18 -0300

    main

--- main.c
+++ main.c
@@ -9,29 +9,36 @@
  */
 int main(void)
 {
-int len;
-int len2;
-unsigned int ui;
-void *addr;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-_printf("Let's try to printf a simple sentence.\n");
-printf("Let's try to printf a simple sentence.\n");
-_printf("Percent:[%%]\n");
-printf("Percent:[%%]\n");
-_printf("Character:[%c]\n", 'H');
-printf("Character:[%c]\n", 'H');
-_printf("Character:[%c]\n", 'M');
-printf("Character:[%c]\n", 'M');
-_printf("");
-printf("");
-_printf("hola: %c Colo y Joaco\n", 'd');
-printf("hola: %c Colo y Joaco\n", 'd');
-_printf("\n");
-printf("\n");
-_printf("finish %s\n", "now");
-printf("finish %s\n", "now");
-_printf("String:[%s]\n", "I am a string !");
-printf("String:[%s]\n", "I am a string !");
-return (0);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
-
