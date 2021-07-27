f63e2e36d3df6c486aa31f3abf10fd58cce3be60
ralexrivero <ralexrivero@gmail.com>
2021-07-27 11:16:27 -0300

    New putchar with 1024 buffer

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,26 @@
-#include <unistd.h>
-
+#include "holberton.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ *_putchar - writes the character c to stdout
+ *@c: The char to print
+ *Return: On succes 1, on error -1
+ *putchar uses a local buffer of 1024 to call write
+ * as little as possible
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int _puthar(char c)
 {
-	return (write(1, &c, 1));
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if  (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
 }
