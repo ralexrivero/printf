b99d02e819b3b1c03d68b0f1e95a301fcca34203
ralexrivero <ralexrivero@gmail.com>
2021-07-27 03:58:41 -0300

    include border cases

--- _printf.c
+++ _printf.c
@@ -11,7 +11,9 @@ int _printf(const char *format, ...)
 	va_list list;
 
 	va_start(list, format);
-	if (!format || (format[0] == '%' && !format[i + 1]))
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 	if (format && format[i])
 	{
