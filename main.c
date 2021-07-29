e528d6eaa118b27297d412ec5bb746cde8fc0ce9
ralexrivero <ralexrivero@gmail.com>
2021-07-28 23:20:16 -0300

    binary is killing me, really

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%b\n", 0);
+    _printf("%b\n", 15);
+    _printf("%b\n", 98);
+    return (0);
+}
--- p_b.c
+++ p_b.c
@@ -7,23 +7,23 @@
  */
 int p_b(va_list p)
 {
-	unsigned int n = va_arg(p, int), binary[1024];
-	int i = 0, j = 0;
+unsigned int n = va_arg(p, int), binary[1024];
+int i = 0, j = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-	while (n > 0);
-	{
-		binary[i] = n % 2;
-		n /= 2;
-		i++;
-	}
-	for(j = i -1; j >= 0; j--)
-	{
-		_putchar(binary[j] + '0');
-	}
-	return (i);
+if (n == 0)
+{
+_putchar('0');
+return (1);
+}
+while (n > 0);
+{
+binary[i] = n % 2;
+n /= 2;
+i++;
+}
+for(j = i -1; j >= 0; j--)
+{
+_putchar(binary[j] + '0');
+}
+return (i);
 }
