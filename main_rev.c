bb6dcda64959c78073c9dfe222b0314174e2f727
ralexrivero <ralexrivero@gmail.com>
2021-07-29 09:53:35 -0300

    main for reverse

--- main_rev.c
+++ main_rev.c
@@ -20,6 +20,8 @@ ui = (unsigned int)INT_MAX + 1024;
 addr = (void *)0x7ffe637541f0;
 len = _printf("Percent:[%%]\n");
 len2 = printf("Percent:[%%]\n");
+_printf("reverse: %r\n", "revesed string");
+_printf("reverse: %r\n"), "123456789";
+_printf("reverse: %r\n", len);
 return (0);
 }
-
