f2adc0d99e544f0a6ed545b231630eb28b6ab116
ralexrivero <ralexrivero@gmail.com>
2021-07-29 13:30:51 -0300

    Dont use other main than main.c

---
+++ test/main_rev.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char len[] = "Hola tu";
+
+_printf("Let's try to printf a simple sentence.\n");
+_printf("reverse:%s: %r\n", "revesed string");
+_printf("reverse:%s %r\n", "123456789");
+_printf("reverse:%r\n", len);
+return (0);
+}
