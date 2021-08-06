9cd8a3e4747cabc96188381c49258c4cd716c501
root <ralexrivero@gmail.com>
2021-08-06 09:05:23 -0700

    hexa

--- p_Xu.c
+++ p_Xu.c
@@ -2,35 +2,35 @@
 
 /**
  * p_Xu - convert decimal to hexadecimal in uppercase
- * @arg: The argument pointer
+ * @p: The argument pointer
  * Description: This function prints a unsigned hexadecimal
  * Return: the length of output
  */
 int p_Xu(va_list p)
 {
-    unsigned long int varULL_step = va_arg(p, int), varULL_temp = 0;
-    char varChr_hexa_temp[1024];
-    int i = 0, count = 0;
+	unsigned long int varULL_step = va_arg(p, int), varULL_temp = 0;
+	char varChr_hexa_temp[1024];
+	int i = 0, count = 0;
 
-    while(varULL_step != 0)
-    {
-        varULL_temp = varULL_step % 16;
-        if (varULL_temp < 10)
-        {
-            varChr_hexa_temp[i] = varULL_temp + 48;
-            i++;
-        }
-        else
-        {
-            varChr_hexa_temp[i] = varULL_temp + 55;
-            i++;
-        }
-        varULL_step /= 16;
-    }
-    for(i = i -1 ; i >= 0; i--)
-    {
-        count += _putchar(varChr_hexa_temp[i]);
-    }
-    return (count);
+	while (varULL_step != 0)
+	{
+	varULL_temp = varULL_step % 16;
+	if (varULL_temp < 10)
+	{
+	varChr_hexa_temp[i] = varULL_temp + 48;
+	i++;
+	}
+	else
+	{
+		varChr_hexa_temp[i] = varULL_temp + 55;
+		i++;
+	}
+	varULL_step /= 16;
+	}
+	for (i = i - 1 ; i >= 0; i--)
+	{
+		count += _putchar(varChr_hexa_temp[i]);
+	}
+	return (count);
 }
 
