ab002fb7b1ea976d3bf4cbdf38544735fe4d634d
ralexrivero <ralexrivero@gmail.com>
2021-08-11 19:58:02 -0400

    fix style

--- main.c
+++ main.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    _printf("%S\n", "Holberton\nSchool");
-    _printf("%S\n", "No special character.");
-    _printf("%S\n", "\n");
-    _printf("%S\n", "\x01\x02\x03\x04\x05\x06\x07");
-    _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
-    _printf("");
-    _printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
-    return (0);
+	_printf("%S\n", "Holberton\nSchool");
+	_printf("%S\n", "No special character.");
+	_printf("%S\n", "\n");
+	_printf("%S\n", "\x01\x02\x03\x04\x05\x06\x07");
+	_printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+	_printf("");
+	_printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
+	return (0);
 }
---
+++ test/main_special_chars.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("%S\n", "Holberton\nSchool");
+    _printf("%S\n", "No special character.");
+    _printf("%S\n", "\n");
+    _printf("%S\n", "\x01\x02\x03\x04\x05\x06\x07");
+    _printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+    _printf("");
+    _printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
+    return (0);
+}
