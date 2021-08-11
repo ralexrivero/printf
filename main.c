7fac4edec8115ba11b5ee3222d8233d8d4c200c6
ralexrivero <ralexrivero@gmail.com>
2021-08-11 19:59:06 -0400

    fix style

--- main.c
+++ main.c
@@ -11,7 +11,8 @@ int main(void)
 	_printf("%S\n", "No special character.");
 	_printf("%S\n", "\n");
 	_printf("%S\n", "\x01\x02\x03\x04\x05\x06\x07");
-	_printf("Could you print some non-prntable characters?\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
+	_printf("Could you print some non-prntable characters?
+	\n%S\nThanks!\n", "Sure:\x1F\x7F\n");
 	_printf("");
 	_printf("- What did you say?\n- %S\n- That's what I thought.\n", "");
 	return (0);
